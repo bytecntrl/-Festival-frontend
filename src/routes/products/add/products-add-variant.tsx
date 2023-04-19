775343a584e9ba1c6669aae56199b1b201c71842
ByteCntrl <bytecntrl@gmail.com>
2023-04-19 22:17:08 +0200

    Update products routes

--- src/routes/products/add/products-add-variant.tsx
+++ src/routes/products/add/products-add-variant.tsx
@@ -17,9 +17,81 @@ function ProductsAddVariant(props: ProductsAddVariantProps) {
         }))
     }
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>, i: number) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        let tmp = props.form.variants;
+
+        switch (name) {
+            case "name":
+                tmp[i].name = value;
+                break;
+            
+            case "price":
+                tmp[i].price = +value;
+                break;
+        }
+
+        props.setForm(values => ({...values, variants: tmp}));
+    }
+
+    const deleteVariant = (i: number) => {
+        let tmp = props.form.variants;
+
+        tmp.splice(i, 1);
+
+        props.setForm(values => ({...values, variants: tmp}));
+    }
+
+    let variants: JSX.Element[] = [];
+
+    props.form.variants.forEach((v, i) => {
+        variants.push(
+            <div className="row mb-2" key={i}>
+					<div className="col-auto">
+						<label htmlFor="NameVariantField" className="visually-hidden">Name</label>
+						<input
+							type="text"
+							className="form-control"
+							id="NameVariantField"
+							placeholder="Enter name of variant"
+                            name="name"
+                            value={v.name}
+                            onChange={(e) => handleChange(e, i)}
+						/>
+					</div>
+					<div className="col-auto">
+						<label htmlFor="PriceVariantField" className="visually-hidden">Price</label>
+						<input
+							type="number"
+							min="0.00"
+							step="0.05"
+							className="form-control"
+							id="PriceVariantField"
+							placeholder="Enter price of variant"
+                            name="price"
+                            value={v.price}
+                            onChange={(e) => handleChange(e, i)}
+						/>
+					</div>
+					<div className="col-auto">
+						<button 
+                            type="button" 
+                            className="btn btn-danger"
+                            onClick={() => deleteVariant(i)}
+                        >
+                            <i className="bi bi-trash" />
+                        </button>
+					</div>
+				</div>
+        );
+    })
+
     return (
         <div className="mb-3">
 			<p className="form-label"><strong>Variants:</strong></p>
+            {variants}
             <button 
                 type="button" 
                 className="btn btn-primary btn-sm"
